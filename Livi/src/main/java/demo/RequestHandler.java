68cab3ee3e5cc894f383f3c1b7033dff1a46f7a1
Yoonsung <lvev9925@naver.com>
2014-12-25 21:02:34 +0900

    Add Crawler4j Library. Get HTML and Text From Test Request.

--- Livi/src/main/java/demo/RequestHandler.java
+++ Livi/src/main/java/demo/RequestHandler.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,8 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RequestHandler {
 	
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	
 	@RequestMapping("/")
 	public @ResponseBody String home() {
+		
+		log.info("homeRequest");
+		try {
+			new BasicCrawlController("/Users/yoon/Desktop", 1);
+		} catch (Exception e) {
+			log.info("CrawlError : {}", e.getMessage());
+			e.printStackTrace();
+		}
 		return "home";
 	}
 	
