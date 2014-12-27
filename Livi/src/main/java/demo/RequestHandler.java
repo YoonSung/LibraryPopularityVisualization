61969214627d4245e3378dbfb005dce0228e51bd
Yoonsung <lvev9925@naver.com>
2014-12-27 15:37:24 +0900

    Delete Crawer4j, Update to Jsoup. Test Code Complete.

--- Livi/src/main/java/demo/RequestHandler.java
+++ Livi/src/main/java/demo/RequestHandler.java
@@ -1,5 +1,9 @@
 package demo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,20 +12,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RequestHandler {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-	
+
 	@RequestMapping("/")
 	public @ResponseBody String home() {
-		
+
 		log.info("homeRequest");
 		try {
-			new BasicCrawlController("/Users/yoon/Desktop", 1);
+			log.info("crawling start");
+			// TODO
+			Document doc = Jsoup.connect("https://github.com/search?o=desc&q=image+crop&ref=searchresults&s=updated&type=Repositories&utf8=true").get();
+			log.info("doc : {},", doc.getAllElements().toString());
+			Elements eles = doc.select(".repo-list-item");
+			log.info("eles.size() : {}", eles.size());
+			
+			for (Element element : eles) {
+				Elements aTags = element.select(".repo-list-stats a");
+				String star = aTags.get(0).text();
+				String fork = aTags.get(1).text();
+				String name = element.select(".repo-list-name a").attr("href");
+				String description = element.select(".repo-list-description").text();
+				String updatedTime = element.select(".repo-list-meta time").text();
+				
+				log.info("start : {}", Integer.parseInt(star.replaceAll(",", "")));
+				log.info("fork : {}", Integer.parseInt(fork.replaceAll(",", "")));	
+				log.info("name : {}", name);
+				log.info("description : {}", description);
+				log.info("updatedTime : {}", updatedTime);
+				log.info("=========================\n\n");
+			}
+
+			log.info("crawling End");
 		} catch (Exception e) {
 			log.info("CrawlError : {}", e.getMessage());
 			e.printStackTrace();
 		}
 		return "home";
 	}
-	
+
 }
