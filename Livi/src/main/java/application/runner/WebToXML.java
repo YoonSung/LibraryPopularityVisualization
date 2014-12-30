9633f0bbbb40169e862640350fb01e93dee48749
Yoonsung <lvev9925@naver.com>
2014-12-30 16:37:36 +0900

    Add Application Properties (External Tomcat, PORT etc)

---
+++ Livi/src/main/java/application/runner/WebToXML.java
@@ -0,0 +1,14 @@
+package application.runner;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+
+public class WebToXML extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return super.configure(application);
+	}
+
+}
